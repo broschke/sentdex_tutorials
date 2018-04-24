6c4f224417df1e115e37d51bc4472117fec34e35
Bernardo Roschke <bernardo_roschke@choicehotels.com>
2018-04-24 09:40:03 -0400

    ml4 video

--- machine_learning/ml4_regression_training_testing.py
+++ machine_learning/ml4_regression_training_testing.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
@@ -13,7 +15,20 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True) #treat nans as outliers
 
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 
 df['label']=df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.head())
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
