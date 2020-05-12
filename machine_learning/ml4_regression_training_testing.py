c811816fd7aa0c703f2523a3fff2a58a906c82b5
Bernardo Roschke <Bernardo.Roschke@choicehotels.com>
2020-05-12 10:01:35 -0400

    updates

--- machine_learning/ml4_regression_training_testing.py
+++ machine_learning/ml4_regression_training_testing.py
@@ -3,8 +3,9 @@ import quandl, math
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+from api_key import key
 
-df = quandl.get('WIKI/GOOGL')
+df = quandl.get("WIKI/GOOGL", api_key=key)
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] *100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] *100
